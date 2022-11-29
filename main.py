15d736474a3b6cf85aa1f3dbf7e5920014ce592d
chenghaoWang <chenghaomartin@gmail.com>
2022-11-29 22:56:41 +0100

    Init commit

--- main.py
+++ main.py
@@ -6,10 +6,7 @@
 
 def generator(class_name, param_list):
     # constructor
-    code = "class " + class_name + ":\n\tdef __init__(self,"
-    for param in param_list:
-        code += param + ","
-    code = code[:-1] + "):\n"
+    code = "class " + class_name + ":\n\tdef __init__(self," + ",".join(param_list) + "):\n"
     for param in param_list:
         code += "\t\tself." + param + " = " + param + "\n"
 
@@ -25,6 +22,8 @@ def generator(class_name, param_list):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    parameter_list = ['fare_class', 'unit_fare_value', 'volume', 'container_type', 'orig', 'dest', 'date_avail', 'date_due', 'cat_customer']
-    my_code = generator("Demand", parameter_list)
+    # parameter_list = ['fare_class', 'unit_fare_value', 'volume', 'container_type', 'orig', 'dest', 'date_avail', 'date_due', 'cat_customer']
+    # my_code = generator("Demand", parameter_list)
+    parameter_list = ['network', 'demand']
+    my_code = generator("DataInstance", parameter_list)
     print(my_code)
